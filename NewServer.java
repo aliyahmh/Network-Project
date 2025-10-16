b72545c336c036f68f8720dbd436878790e34c5b
Aliyah <115356580+aliyahmh@users.noreply.github.com>
2025-10-16 21:42:40 +0300

    lab classes

---
+++ NewServer.java
@@ -0,0 +1,24 @@
+import java.io.*; 
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+
+// Server class 
+class NewServer { 
+    private static ArrayList<NewClient> clients=new ArrayList<>();
+    public static void main(String[] args) throws IOException
+    {
+        ServerSocket serverSocket = new ServerSocket(9090);
+
+        while (true){
+         System.out.println("Waiting for client connection");
+         Socket client=serverSocket.accept();
+         System.out.println("Connected to client");
+         NewClient clientThread=new NewClient(client,clients); // new thread 
+         clients.add(clientThread);
+         new Thread (clientThread).start();
+         
+        }
+    }
+}
