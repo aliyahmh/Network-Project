3e3ece7c5a6068911d6b4e0aff47e6f99caa7b1b
Aliyah <115356580+aliyahmh@users.noreply.github.com>
2025-10-19 18:24:24 +0300

    a

--- client.java
+++ client.java
@@ -1,33 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+
+import java.io.*; 
+import java.net.*; 
+import java.util.*; 
 
 
-public class Client {
+class Client { 
     
-    private static final String Server_IP="localhost";
-    private static final int Server_port=9090;
- 
-        public static void main(String[] args) throws IOException{
-          try(Socket socket = new Socket (Server_IP,Server_port)) {
-              ServerConnection servcon=new ServerConnection(socket);
-              BufferedReader keyboard=new BufferedReader (new InputStreamReader(System.in));
-              PrintWriter out=new PrintWriter(socket.getOutputStream(),true);
-              new Thread (servcon).start(); 
-              try{
-                  while(true){
-                      System.out.println("> ");
-                      String command=keyboard.readLine();                     
-                      if(command.equals("quit")) break;
-                      out.println(command); 
-                  } // end of while loop
-              } catch (Exception e){
-                  e.printStackTrace();
-              }
-          }
-              System.exit(0); 
-        }
-}
+    // driver code 
+    public static void main(String[] args) 
+    { 
+        // establish a connection by providing host and port 
+        // number 
+        try (Socket socket = new Socket("localhost", 1234)) { 
+            
+            // writing to server 
+            PrintWriter out = new PrintWriter( 
+                socket.getOutputStream(), true); 
+  
+            // reading from server 
+            BufferedReader in 
+                = new BufferedReader(new InputStreamReader( 
+                    socket.getInputStream())); 
+  
+            // object of scanner class 
+            Scanner sc = new Scanner(System.in); 
 
+            String line = null; 
+  
+            while (!"exit".equalsIgnoreCase(line)) { 
+                
+                // reading from user 
+                line = sc.nextLine(); 
+  
+                // sending the user input to server 
+                out.println(line); 
+                out.flush(); 
+  
+                // displaying server reply 
+                System.out.println("Server replied "
+                                   + in.readLine()); 
+            } 
+            
+            // closing the scanner object 
+            sc.close(); 
+        } 
+        catch (IOException e) { 
+            e.printStackTrace(); 
+        } 
+    } 
+}
