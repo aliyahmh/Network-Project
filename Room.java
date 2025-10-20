53da48153c0f375d346cb59ea0a3fb6401629f52
Hawra-10 <alhammadhawra@gmail.com>
2025-10-20 12:42:04 +0300

    h

--- Room.java
+++ Room.java
@@ -1,27 +1,27 @@
 public class Room {
     private String type;
-    private String name;
+    private String ID;
     private String day;
     private String time;
     private boolean available;
 
-    public Room(String type, String name, String day, String time) {
+    public Room(String type, String id, String day, String time) {
         this.type = type;
-        this.name = name;
+        this.ID = id;
         this.day = day;
         this.time = time;
         this.available = true;
     }
 
     public String getType() { return type; }
-    public String getName() { return name; }
+    public String getName() { return ID; }
     public String getDay() { return day; }
     public String getTime() { return time; }
     public boolean isAvailable() { return available; }
     public void setAvailable(boolean available) { this.available = available; }
 
     public String toString() {
-        return name + " (" + type + ") - " + day + " (" + time + ")";
+        return ID + " (" + type + ") - " + day + " (" + time + ")";
     }
 }
 
--- client.java
+++ client.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class Client {
+public class client {
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 1234);
